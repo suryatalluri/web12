e0d3d4b797823dbb407feb93a888298e1e163f41
Mani null <surya.talluri+6@wavemaker.com>
2016-06-29 11:08:58 +0000

    New files added - [EmployeeServiceImpl.java,Livelist.variables.json,HrdbQueryExecutorService.java,User.java,Department.java,hrdb_procedure.json,servicedef.xml,DepartmentService.java,DepartmentServiceImpl.java,hrdb.properties,DepartmentController.java,hrdb.log,UserService.java,Employee.java,hrdb_published_dataModel.json,package-info.java,UserServiceImpl.java,Livelist.html,hrdb-queries.hbm.xml,VacationController.java,hrdb.spring.xml,hrdb_query.json,VacationDao.java,HrdbProcedureExecutorService.java,UserController.java,EmployeeDao.java,Livelist.css,hrdb.script,QueryExecutionController.java,hrdb-procedures.mappings.json,DepartmentDao.java,ProcedureExecutionController.java,db-connection-settings.json,VacationServiceImpl.java,EmployeeService.java,hrdb.properties,HrdbQueryExecutorServiceImpl.java,VacationService.java,HrdbProcedureExecutorServiceImpl.java,Livelist.js,EmployeeController.java,UserDao.java,Vacation.java,Livelist] Files modified - [Main.variables.json,pages-config.json,Main.html,production.properties,Main.js,project-services.xml,pom.xml,app.variables.json,development.properties] Refactored files - [] Fixed bugs - [] Removed files - []

--- src/main/webapp/pages/Livelist/Livelist.js
+++ src/main/webapp/pages/Livelist/Livelist.js
@@ -1,4 +1,4 @@
-Application.$controller("MainPageController", ["$scope", function ($scope) {
+Application.$controller("LivelistPageController", ["$scope", function ($scope) {
     "use strict";
 
     /* perform any action on widgets/variables within this block */
@@ -7,7 +7,7 @@ Application.$controller("MainPageController", ["$scope", function ($scope) {
          * variables can be accessed through '$scope.Variables' property here
          * e.g. to get dataSet in a staticVariable named 'loggedInUser' use following script
          * $scope.Variables.loggedInUser.getData()
-         * 
+         *
          * widgets can be accessed through '$scope.Widgets' property here
          * e.g. to get value of text widget named 'username' use following script
          * '$scope.Widgets.username.datavalue'
@@ -15,17 +15,3 @@ Application.$controller("MainPageController", ["$scope", function ($scope) {
     };
 
 }]);
-
-Application.$controller("grid1Controller", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
-]);
-
-Application.$controller("liveform1Controller", ["$scope",
-	function($scope) {
-		"use strict";
-		$scope.ctrlScope = $scope;
-	}
-]);
