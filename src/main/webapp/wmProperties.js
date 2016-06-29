739a2c627803dac6cb68ce3d8879e648bec7ea21
root <root@d1603208c4d0>
2016-06-29 10:48:28 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "defaultLanguage" : "en",
+  "displayName" : "web12",
+  "homePage" : "Main",
+  "name" : "web12",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
